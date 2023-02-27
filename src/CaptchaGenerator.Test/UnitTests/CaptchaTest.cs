a841bd68b24cf446a98c082031f71f4c96523e82
bezzad <behzad.khosravifar@gmail.com>
2023-02-27 16:46:21 +0330

    Added some unit tests

---
+++ src/CaptchaGenerator.Test/UnitTests/CaptchaTest.cs
@@ -0,0 +1,100 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using Xunit;
+
+namespace CaptchaGenerator.Test.UnitTests
+{
+    public class CaptchaTest : Captcha
+    {
+        public CaptchaTest() : base(new CaptchaOptions()) { }
+
+        [Fact]
+        public void ImageSizeTest()
+        {
+            var width = 10;
+            var height = 10;
+
+            // act
+            using var img = new Image<Rgba32>(width, height);
+
+            // assert
+            Assert.Equal(width, img.Width);
+            Assert.Equal(height, img.Height);
+        }
+
+        [Fact]
+        public void AddNoisePointTest()
+        {
+            // arrange
+            var width = 10;
+            var height = 10;
+            var pixels = new Rgba32[width * height].AsSpan();
+            var noiseColor = Color.Black.ToPixel<Rgba32>();
+            var backgroundColor = Color.White.ToPixel<Rgba32>();
+            var noiseHistogram = 0;
+            var backgroundHistogram = 0;
+            _options.NoiseRateColor = new[] { Color.Black };
+            using var img = new Image<Rgba32>(width, height);
+
+            // act
+            img.Mutate(ctx => ctx.BackgroundColor(backgroundColor));
+            AddNoisePoint(img);
+            img.CopyPixelDataTo(pixels);
+            foreach (var pixel in pixels)
+            {
+                if (pixel.Equals(noiseColor))
+                {
+                    noiseHistogram++;
+                }
+                else if (pixel.Equals(backgroundColor))
+                {
+                    backgroundHistogram++;
+                }
+            }
+
+            // assert
+            Assert.Equal(1, noiseHistogram);
+            Assert.Equal(width * height - 1, backgroundHistogram);
+        }
+
+        [Fact]
+        public void DrawNoiseLinesTest()
+        {
+            // arrange
+            var width = 10;
+            var height = 10;
+            var pixels = new Rgba32[width * height].AsSpan();
+            var noiseColor = Color.Black.ToPixel<Rgba32>();
+            var backgroundColor = Color.White.ToPixel<Rgba32>();
+            var noiseHistogram = 0;
+            var backgroundHistogram = 0;
+            _options.DrawLinesColor = new[] { Color.Black };
+            _options.MinLineThickness = 1;
+            _options.MaxLineThickness = 1;
+            using var img = new Image<Rgba32>(width, height);
+
+            // act
+            img.Mutate(ctx => ctx.BackgroundColor(backgroundColor));
+            DrawNoiseLines(img);
+            img.CopyPixelDataTo(pixels);
+            foreach (var pixel in pixels)
+            {
+                if (pixel.Equals(backgroundColor))
+                {
+                    backgroundHistogram++;
+                }
+                else
+                {
+                    noiseHistogram++;
+                }
+            }
+
+            // assert
+            Assert.True(2 <= noiseHistogram, "noise histogram is less than 2");
+            Assert.Equal(width * height - noiseHistogram, backgroundHistogram);
+            Assert.True(2 <= backgroundHistogram, "background histogram is less than 2");
+        }
+    }
+}
--- src/CaptchaGenerator/Captcha.cs
+++ src/CaptchaGenerator/Captcha.cs
@@ -10,7 +10,7 @@ namespace CaptchaGenerator
     public class Captcha : ICaptchaModule
     {
         private static Random Rand = new Random(DateTime.Now.GetHashCode());
-        private readonly CaptchaOptions _options;
+        protected readonly CaptchaOptions _options;
 
         public Captcha(CaptchaOptions options)
         {
@@ -20,49 +20,47 @@ namespace CaptchaGenerator
         public byte[] Generate(string text)
         {
             byte[] result;
-            using (var imgText = new Image<Rgba32>(_options.Width, _options.Height))
+            var position = 5f;
+            var charPadding = (byte)Rand.Next(5, 10);
+            var fontName = _options.FontFamilies[Rand.Next(0, _options.FontFamilies.Length)];
+            var font = SystemFonts.CreateFont(fontName, _options.FontSize, _options.FontStyle);
+            using var imgText = new Image<Rgba32>(_options.Width, _options.Height);
+            imgText.Mutate(ctx => ctx.BackgroundColor(_options.BackgroundColor[Rand.Next(0, _options.BackgroundColor.Length)]));
+
+            foreach (char ch in text)
             {
-                float position = 5;
-                byte charPadding = (byte)Rand.Next(5, 10);
-                imgText.Mutate(ctx => ctx.BackgroundColor(_options.BackgroundColor[Rand.Next(0, _options.BackgroundColor.Length)]));
-
-                string fontName = _options.FontFamilies[Rand.Next(0, _options.FontFamilies.Length)];
-                Font font = SystemFonts.CreateFont(fontName, _options.FontSize, _options.FontStyle);
-
-                foreach (char ch in text)
-                {
-                    var location = new PointF(charPadding + position, Rand.Next(6, Math.Abs(_options.Height - _options.FontSize - 5)));
-                    imgText.Mutate(ctx => ctx.DrawText(ch.ToString(), font, _options.TextColor[Rand.Next(0, _options.TextColor.Length)], location));
-                    position += TextMeasurer.Measure(ch.ToString(), new TextOptions(font)).Width;
-                }
-
-                // add rotation
-                AffineTransformBuilder rotation = GetRotation();
-                imgText.Mutate(ctx => ctx.Transform(rotation));
-
-                // add the dynamic image to original image
-                var size = (ushort)TextMeasurer.Measure(text, new TextOptions(font)).Width;
-                var img = new Image<Rgba32>(size + 15, _options.Height);
-                img.Mutate(ctx => ctx.BackgroundColor(_options.BackgroundColor[Rand.Next(0, _options.BackgroundColor.Length)]));
-                img.Mutate(ctx => ctx.DrawImage(imgText, 0.80f));
-
-                for (var i = 0; i < _options.DrawLines; i++)
-                    DrawNoiseLines(img);
-
-                for (var i = 0; i < _options.NoiseRate; i++)
-                    AddNoisePoint(img);
-
-                img.Mutate(x => x.Resize(_options.Width, _options.Height));
-
-                using var ms = new MemoryStream();
-                img.Save(ms, _options.Encoder);
-                result = ms.ToArray();
+                var location = new PointF(charPadding + position, Rand.Next(6, Math.Abs(_options.Height - _options.FontSize - 5)));
+                imgText.Mutate(ctx => ctx.DrawText(ch.ToString(), font, _options.TextColor[Rand.Next(0, _options.TextColor.Length)], location));
+                position += TextMeasurer.Measure(ch.ToString(), new TextOptions(font)).Width;
             }
+
+            // add rotation
+            AffineTransformBuilder rotation = GetRotation();
+            imgText.Mutate(ctx => ctx.Transform(rotation));
+
+            // add the dynamic image to original image
+            var size = (ushort)TextMeasurer.Measure(text, new TextOptions(font)).Width;
+            using var img = new Image<Rgba32>(size + 15, _options.Height);
+            img.Mutate(ctx => ctx.BackgroundColor(_options.BackgroundColor[Rand.Next(0, _options.BackgroundColor.Length)]));
+            img.Mutate(ctx => ctx.DrawImage(imgText, 0.80f));
+
+            for (var i = 0; i < _options.DrawLines; i++)
+                DrawNoiseLines(img);
+
+            for (var i = 0; i < _options.NoiseRate; i++)
+                AddNoisePoint(img);
+
+            img.Mutate(x => x.Resize(_options.Width, _options.Height));
+
+            using var ms = new MemoryStream();
+            img.Save(ms, _options.Encoder);
+            result = ms.ToArray();
+
 
             return result;
         }
 
-        private void AddNoisePoint(Image img)
+        protected void AddNoisePoint(Image img)
         {
             int x0 = Rand.Next(0, img.Width);
             int y0 = Rand.Next(0, img.Height);
@@ -70,18 +68,19 @@ namespace CaptchaGenerator
                     1, new PointF[] { new Vector2(x0, y0), new Vector2(x0, y0) }));
         }
 
-        private void DrawNoiseLines(Image img)
+        protected void DrawNoiseLines(Image img)
         {
-            int x0 = Rand.Next(0, Rand.Next(0, 30));
-            int y0 = Rand.Next(10, img.Height);
+            int x0 = Rand.Next(0, Rand.Next(0, Math.Min(30, img.Width)));
+            int y0 = Rand.Next(Math.Min(10, img.Height), img.Height);
             int x1 = Rand.Next(img.Width - Rand.Next(0, (int)(img.Width * 0.25)), img.Width);
             int y1 = Rand.Next(0, img.Height);
-            img.Mutate(ctx => ctx.DrawLines(_options.DrawLinesColor[Rand.Next(0, _options.DrawLinesColor.Length)],
-                          Extensions.GenerateNextFloat(_options.MinLineThickness, _options.MaxLineThickness),
+            var thickness = Extensions.GenerateNextFloat(_options.MinLineThickness, _options.MaxLineThickness);
+            var lineColor = _options.DrawLinesColor[Rand.Next(0, _options.DrawLinesColor.Length)];
+             img.Mutate(ctx => ctx.DrawLines(lineColor, thickness,
                           new PointF[] { new PointF(x0, y0), new PointF(x1, y1) }));
         }
 
-        private AffineTransformBuilder GetRotation()
+        protected AffineTransformBuilder GetRotation()
         {
             var builder = new AffineTransformBuilder();
             var width = Rand.Next(10, _options.Width);
