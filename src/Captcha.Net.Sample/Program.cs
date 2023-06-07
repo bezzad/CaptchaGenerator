cd0a286785d2b83ac7ea439092fd8fdd1d6ebe49
bezzad <behzad.khosravifar@gmail.com>
2023-06-07 14:26:00 +0330

    added metrica to sample generator

--- src/Captcha.Net.Sample/Program.cs
+++ src/Captcha.Net.Sample/Program.cs
@@ -3,17 +3,24 @@ using System.Diagnostics;
 
 var captchaGenerator = new CaptchaGenerator();
 var currentDirectory = Directory.CreateDirectory("captcha");
+var count = 1000;
+var times = new List<long>(count);
+var stopWatch = new Stopwatch();
 
-for (int i = 0; i < 1000; i++)
+for (int i = 0; i < count; i++)
 {
+    stopWatch.Restart();
     var key = captchaGenerator.GenerateCaptchaCode();
     var result = captchaGenerator.GenerateCaptchaImage(200, 100, key);
     Console.WriteLine($"Captcha {i}: \n");
     Console.WriteLine(result.CaptchBase64Data);
     Console.WriteLine();
-
+    stopWatch.Stop();
+    times.Add(stopWatch.ElapsedMilliseconds);
     File.WriteAllBytes($"{currentDirectory.FullName}/captcha-{i}.png", result.CaptchaByteData);
 }
+
+Console.WriteLine($"Duration:  Max({times.Max()}ms),  Min({times.Min()}ms),  Average({times.Average()}ms)");    
 
 // open the directory after completion
 if (OperatingSystem.IsWindows())
